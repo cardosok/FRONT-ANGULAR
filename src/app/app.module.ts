ee81ebc7336d54e2392408adb11a038bd142592f
Daniel Nascimento <1744966@TCS.com>
2019-10-08 13:12:17 -0300

    Thi is a commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MatSliderModule } from '@angular/material/slider';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatCardModule } from '@angular/material/card';
+// import { Chart } from 'canvasjs';
+// import { Chart } from 'chart.js';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+
+    MatSliderModule,
+    FlexLayoutModule,
+    MatCardModule,
+    // Chart
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
+
---
+++ src/app/index/index.component.ts
@@ -0,0 +1,86 @@
+import { Component, OnInit } from '@angular/core';
+// import * as CanvasJS from './canvasjs.min';
+// import { Chart } from 'canvasjs';
+import { Chart } from 'chart.js';
+
+@Component({
+  selector: 'app-index',
+  templateUrl: './index.component.html',
+  styleUrls: ['./index.component.css']
+})
+
+export class IndexComponent implements OnInit {
+  chart = [];
+  constructor() { }
+
+  ngOnInit() {
+    var ctx = document.getElementById('myChart');
+
+    let temp_max = [80, 90, 100, 60];
+    let temp_min = [15, 20, 30, 50];
+    this.chart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: ["lbl1", "lbl2", "lbl3", "lbl4"],
+        datasets: [
+          {
+            data: temp_max,
+            borderColor: '#3cba9f',
+            fill: false
+          },
+          {
+            data: temp_min,
+            borderColor: '#ffcc00',
+            fill: false
+          },
+        ]
+      },
+      options: {
+        legend: { display: false },
+        scales: {
+          ticks: { beginAtZero: true },
+          xAxes: [{ display: true }],
+          yAxes: [{ display: true }]
+        }
+      }
+    })
+
+    var ctx2 = document.getElementById('myChart2');
+
+    var myChart2 = new Chart(ctx2, {
+      type: 'bar',
+      data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+          label: '# of Votes',
+          data: [12, 19, 3, 57, 2, 30],
+          backgroundColor: [
+            'rgba(255, 99, 132, 0.2)',
+            'rgba(54, 162, 235, 0.2)',
+            'rgba(255, 206, 86, 0.2)',
+            'rgba(75, 192, 192, 0.2)',
+            'rgba(153, 102, 255, 0.2)',
+            'rgba(255, 159, 64, 0.2)'
+          ],
+          borderColor: [
+            'rgba(255, 99, 132, 1)',
+            'rgba(54, 162, 235, 1)',
+            'rgba(255, 206, 86, 1)',
+            'rgba(75, 192, 192, 1)',
+            'rgba(153, 102, 255, 1)',
+            'rgba(255, 159, 64, 1)'
+          ],
+          borderWidth: 1
+        }]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: { beginAtZero: true }
+          }]
+        }
+      }
+    });
+
+  }
+}
